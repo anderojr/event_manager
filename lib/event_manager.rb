a88ee95f885fc48364d3e788dd89864e7fa862cb
anderojr <and.okuda@gmail.com>
2023-05-25 01:41:13 -0300

    refactored legislators functions to please rubocop

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require_relative 'config'
@@ -11,17 +13,28 @@ def legislators_by_zipcode(zip)
   civic_info.key = API_KEY
 
   begin
-    legislators = civic_info.representative_info_by_address(
-      address: zip,
-      levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
-  rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    legislators = fetch_legislators(civic_info, zip)
+    format_legislator_names(legislators)
+  rescue StandardError
+    handle_error
   end
+end
+
+def fetch_legislators(civic_info, zip)
+  civic_info.representative_info_by_address(
+    address: zip,
+    levels: 'country',
+    roles: %w[legislatorUpperBody legislatorLowerBody]
+  ).officials
+end
+
+def format_legislator_names(legislators)
+  legislator_names = legislators.map(&:name)
+  legislator_names.join(', ')
+end
+
+def handle_error
+  'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
 end
 
 puts 'Event Manager Initialized!'
