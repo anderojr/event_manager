792f9fe05af8f1f9a8e8f46fc0883962cee42771
anderojr <and.okuda@gmail.com>
2023-05-25 02:43:12 -0300

    finalized assingnments

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -42,6 +42,44 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def clean_phone_numbers(phone_number)
+  phone_number = phone_number.gsub(/[()\-,. ]/, '')
+
+  if phone_number.length == 11 && phone_number[0] == '1'
+    phone_number = phone_number[1..10]
+  elsif phone_number.length != 10
+    phone_number = nil
+  end
+
+  puts phone_number
+end
+
+def most_common_reg_day(csv_file)
+  reg_day_frequency = Hash.new(0)
+
+  CSV.foreach(csv_file) do |row|
+    reg_date = row[:regdate]
+    parsed_date = DateTime.strptime(reg_date, '%m/%d/%y %H:%M')
+    reg_day = parsed_date.strftime('%A')
+    reg_day_frequency[reg_day] += 1
+  end
+
+  reg_day_frequency.max_by { |_day, count| count }[0] # most common day
+end
+
+def most_common_hour(csv_file)
+  reg_hour_frequency = Hash.new(0)
+
+  CSV.foreach(csv_file) do |row|
+    reg_date = row[:regdate]
+    parsed_date = DateTime.strptime(reg_date, '%m/%d/%y %H:%M')
+    reg_hour = parsed_date.hour
+    reg_hour_frequency[reg_hour] += 1
+  end
+
+  reg_hour_frequency.max_by { |_hour, count| count }[0] # most common hour
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
